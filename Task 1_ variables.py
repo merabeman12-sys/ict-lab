d9ed339dc09f7e88bf692715e81e1be9aa0bacfd
tahirchaudharyy7-hub <tahirchaudharyy7@gmail.com>
2025-09-22 21:19:23 +0500

    Add files via upload

---
+++ Task 1_ variables.py
@@ -0,0 +1,11 @@
+name = "Amman"
+age = 18
+height = 5.4
+is_student= True
+print(name)
+print("Age:",age)
+print("Height:",height)
+print("Student:",is_student)
+
+
+
---
+++ task 3_grading.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter your marks (0-100):"))
+if marks >= 90:
+    grade = "A"
+elif marks>= 75 and marks < 90: 
+    grade = "B"
+elif marks >= 50 and marks < 75:
+    grade = "C"
+elif marks < 50 and marks >=0:
+    grade = "F"
+else: 
+    print ("Invalid Input")
+    
+print ("Your grade is:",grade)
+  
